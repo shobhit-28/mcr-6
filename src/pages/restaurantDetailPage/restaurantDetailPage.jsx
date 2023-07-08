17890ec63207da74fa0e39f45d41777a1fb21452
shobhit-28 <shobhitraj34@gmail.com>
2023-07-08 22:25:18 +0530

    done

--- src/pages/restaurantDetailPage/restaurantDetailPage.jsx
+++ src/pages/restaurantDetailPage/restaurantDetailPage.jsx
@@ -1,13 +1,13 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { DataContext } from "../../context/dataContext"
 import { useContext, useState } from "react"
-import {AiOutlineCloseCircle, AiOutlineStar} from 'react-icons/ai'
-import {IoArrowBack} from 'react-icons/io5'
+import { AiOutlineCloseCircle, AiOutlineStar } from 'react-icons/ai'
+import { IoArrowBack } from 'react-icons/io5'
 
 export const RestaurantDetailPage = () => {
   const { restaurantID } = useParams()
 
-  const { data } = useContext(DataContext)
+  const { data, setData } = useContext(DataContext)
 
   const restaurantData = data.find(({ id }) => id === Number(restaurantID))
 
@@ -19,7 +19,27 @@ export const RestaurantDetailPage = () => {
     comment: "",
     revName: "Anonymous",
     pp: "https://picsum.photos/150/150"
-})
+  })
+
+  const submitHandler = () => {
+    if (reviewData.comment.length !== 0) {
+      setData(data.map((resData) => resData.id === Number(restaurantID)
+        ?
+        { ...resData, ratings: [...resData.ratings, reviewData] }
+        :
+        resData
+      ))
+      setIsModalOpen(false)
+      setReviewData({
+        rating: 1,
+        comment: "",
+        revName: "Anonymous",
+        pp: "https://picsum.photos/150/150"
+      })
+    } else {
+      alert('Comment cannot be empty')
+    }
+  }
 
   return (
     <div className="bg-pink-50 min-h-screen overflow-auto px-80">
@@ -54,18 +74,18 @@ export const RestaurantDetailPage = () => {
           <hr className="border-pink-300 m-5" />
         </div>
       ))}
-      {isModalOpen && 
+      {isModalOpen &&
         <div className="fixed h-screen w-screen top-0 right-0 z-10 flex justify-center items-center" onClick={() => setIsModalOpen(false)}>
           <div className="opacity-100 bg-pink-800 relative p-5 text-pink-50 rounded-xl w-2/4" onClick={(event) => event.stopPropagation()}>
             <button className="absolute top-2 left-2 text-lg" onClick={() => setIsModalOpen(false)}><AiOutlineCloseCircle /></button>
             <p className="text-center text-2xl font-medium mb-4">Add your review</p>
             <label htmlFor="comment" className="flex my-2 justify-between">
               <p className="">Name:</p>
-              <input type="text" name="comment" id="comment" className="w-2/4 bg-pink-600" />
+              <input type="text" name="comment" id="comment" className="w-2/4 bg-pink-600" onChange={(event) => setReviewData({ ...reviewData, revName: event.target.value })} />
             </label>
             <label htmlFor="select" className="flex justify-between">
               <p className="">Rating:</p>
-              <select name="select" id="select" className="bg-pink-600 rounded w-2/4">
+              <select name="select" id="select" className="bg-pink-600 rounded w-2/4" onChange={(event) => setReviewData({ ...reviewData, rating: event.target.value })}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -75,9 +95,9 @@ export const RestaurantDetailPage = () => {
             </label>
             <label htmlFor="comment" className="flex my-2 justify-between">
               <p className="">Comment:</p>
-              <input type="text" name="comment" id="comment" className="w-2/4 bg-pink-600" />
+              <input type="text" name="comment" id="comment" className="w-2/4 bg-pink-600" onChange={(event) => setReviewData({ ...reviewData, comment: event.target.value })} />
             </label>
-            <button className="m-auto block border-2 p-2 mt-5">Submit</button>
+            <button className="m-auto block border-2 p-2 mt-5" onClick={() => submitHandler()}>Submit</button>
           </div>
         </div>
       }
